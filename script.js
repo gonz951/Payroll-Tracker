3e9b0c3a2799e9ffa934b6068910e237308ef046
Fel <felgonzalez951@gmail.com>
2024-03-21 22:43:25 -0600

    more math.random fails

--- script.js
+++ script.js
@@ -90,12 +90,15 @@ const getRandomEmployee = function(employeesArray) {
   console.log(employeesArray[0])
   console.log(employeesArray.length)
 
-  for (let i = 0; i < employeesArray.length; i++) {
-    let pick = employeesArray[i].firstName;
-    return Math.floor(Math.random(pick) * employeesArray.length);
-  }
+  
 
-  //return Math.floor(Math.random(pick) * employeesArray.length);
+  // let pick = employeesArray[i].firstName;
+  // for (let i = 0; i < employeesArray.length; i++) {
+  //   let pick = employeesArray[i].firstName;
+  //   const 
+  // }
+  // console.log()
+  // return Math.floor(Math.random(pick) * employeesArray.length);
   // let randomNumber = (employeesArray[i]);
   // employeesArray[randomNumber]
   //randomNumber= Rounded down Math.Random times the lenght of the array. 
